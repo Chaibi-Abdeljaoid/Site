91199e8a01bf390e748bb0fe617b78bc367b6a98
Chaibi-Abdeljaoid <abdeljaoidchaibi@gmail.com>
2022-03-26 11:09:27 +0100

    creat projrct

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+ registerForm = new FormGroup({
+   
+  });
+ submitted = false;
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit() {
+        this.registerForm = this.formBuilder.group({
+    
+            email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+            password: ['', [Validators.required, Validators.minLength(6)]]
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
+            return;
+        }
+
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
+    }
+    
+    getData(x:any){
+        console.log(x);}
+}
+
